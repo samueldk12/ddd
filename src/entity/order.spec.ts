db1da92e6e5223eb5228746aebd7efad8f4015be
Samuel Apolinário Arão <samuel.arao@to-brasil.local>
2024-03-19 21:55:04 -0300

    adicionado teste order

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -1,6 +1,26 @@
 import Order from "./order";
+import OrderItem from "./order_item";
 
 describe("Order unit tests", () => {
 
+    it("should throw error when name is empty", ()=>{
+        expect(()=>{
+            let order = new Order("", "123",[]);
+        }).toThrowError("Item qtd must be greater than zero");
+    });
     
-}
+    it("should calculate total", ()=> {
+        const item = new OrderItem("i1", "Item 1", 100, 1);
+        const item2 = new OrderItem("i2", "Item 2", 200, 1);
+        const order = new Order("o1", "c1", [item]);
+
+        const total_order1 = order.total();
+
+        expect(total_order1).toBe(100);
+
+        const order2 = new Order("o1", "c1", [item, item2])
+        const total_order2 = order2.total();
+
+        expect(total_order2).toBe(300)
+    })
+});
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -2,20 +2,25 @@ import OrderItem from "./order_item";
 
 export default class Order{
 
-    _id: string;
-    _costumerId: string;
-    _items: OrderItem[];
+    private _id: string;
+    private _costumerId: string;
+    private _items: OrderItem[];
+    private _total: number;
 
     constructor(id: string, costumerId: string, items: OrderItem[]) {
         this._id = id;
         this._costumerId = costumerId;
         this._items = items;
-        this.validade();
+        this._total = this.total();
     }
 
-    validade(){
-        if(this._costumerId === '' || this._id === ''){
-            throw new Error('Order invalid');
+    validate(): boolean {
+        if(this._id.length === 0){
+            throw new Error("Id is required");
         }
+        return true;
+    }
+    total(): number{
+        return this._items.reduce((acc, item) => acc + item._price, 0)
     }
 }
